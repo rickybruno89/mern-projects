d4106c0dc75da8829228d3d9ce44d4dd43912eff
Ricky <ricardobruno_89@hotmail.com>
2021-02-10 12:53:47 -0300

    working on cotizadorSeguro

--- src/helpers.js
+++ src/helpers.js
@@ -7,3 +7,30 @@ export const revisarPresupuesto = (presupuesto, restante) => {
     return "alert alert-success";
   }
 };
+
+export const obtenerDiferenciaYear = year => {
+  return new Date().getFullYear() - year;
+}
+
+export const calcularMarcaAuto = marca => {
+  let incremento;
+  switch (marca) {
+    case 'europeo':
+      incremento = 1.30
+      break;
+    case 'americano':
+      incremento = 1.15
+      break;
+    case 'asiatico':
+      incremento = 1.05
+      break;
+    default:
+      break;
+  }
+
+  return incremento;
+}
+
+export const obtenerPlanSeguro = plan => {
+  return (plan === 'basico') ? 1.20 : 1.50;
+}
--- src/pages/7-cotizadorSeguro/CotizadorSeguroAutomovil.js
+++ src/pages/7-cotizadorSeguro/CotizadorSeguroAutomovil.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import Header from '../../components/7-cotizadorSeguroAutomovil/Header';
+import Formulario from '../../components/7-cotizadorSeguroAutomovil/Formulario';
+
+const Contenedor = styled.div`
+max-width: 600px;
+margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+    background-color: #FFFFFF;
+    padding: 3rem;
+`
 
 const CotizadorSeguroAutomovil = () => {
     return (
-        <div>
-            Cotizador automovil
+        <div className="cotizadorSeguro">
+            <Contenedor>
+                <Header titulo="Cotizador de Seguros de Automóvil" />
+                <ContenedorFormulario>
+                    <Formulario />
+                </ContenedorFormulario>
+            </Contenedor>
         </div>
     )
 }
